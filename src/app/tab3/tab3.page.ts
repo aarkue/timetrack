1c66343197f7f218e3b53efe2b88f833e84bae31
Aaron Küsters <gitlab@aarkue.eu>
2021-07-04 11:38:08 +0200

    import/export functionality

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ToastController } from '@ionic/angular';
+import { Plugins } from '@capacitor/core';
+import { IonInput, ToastController } from '@ionic/angular';
 import { AccountService } from '../services/account.service';
 import { GamificationService } from '../services/gamification.service';
 
+const { Storage } = Plugins;
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -12,6 +14,19 @@ import { GamificationService } from '../services/gamification.service';
 export class Tab3Page {
   loginForm: FormGroup;
 
+  private readonly localKeys : string[] = 
+  // Time Tracker
+  ["activities","timeTracked",
+  // Pomodoro Options
+      "shortBreakSeconds","longBreakSeconds","workSessionReward","showSeconds","showProgressBar","autoStart","autoFinish",
+  // Pomodoro
+      "timePassedBefore", "status", "paused", "pomodoros", "currDuration", "timerHistory"]
+
+  @ViewChild('fileInputRef')
+  public fileInput : ElementRef<HTMLInputElement>;
+
+  @ViewChild('exportDownload')
+  private exportDownload : ElementRef<HTMLAnchorElement>;
   constructor(public accountService : AccountService,
               public toastController: ToastController, 
               public formBuilder: FormBuilder,
@@ -75,4 +90,53 @@ export class Tab3Page {
     }
   }
 
+
+  importFile(){
+    let file = this.fileInput.nativeElement.files[0];
+    if(file){
+      let reader = new FileReader();
+      reader.onload = async (evt) => {
+        let res = JSON.parse(evt.target.result+"");
+        for(const key in res){
+          if(this.localKeys.indexOf(key) > -1){
+            await Storage.set({key: key, value: res[key]})
+          }
+        }
+        const toastNot = await  this.toastController.create({
+          header: "Import successful! Restart app to see changes.",
+          position: "middle",
+          color: "success",
+          duration: 1500,
+          buttons: [{text: " Ok", icon: "checkmark-outline", role: "cancel", handler: () => {}}],
+        });
+      toastNot.present();
+      }
+      reader.onerror = async (evt) => {
+        const toastNot = await  this.toastController.create({
+          header: "Import failed",
+          position: "middle",
+          color: "danger",
+          duration: 3000,
+          buttons: [{text: " Ok", icon: "checkmark-outline", role: "cancel", handler: () => {}}],
+        });
+      }
+
+      reader.readAsText(file);
+
+    }
+  }
+
+  async exportFile(){
+    let dict = {};
+    for (const key of this.localKeys){
+      dict[key] = (await Storage.get({key: key})).value;
+    }
+    console.log(dict)
+    let json = JSON.stringify(dict);
+    console.log(json);
+    let toExport = new Blob([json],{type: 'text/plain'});
+    this.exportDownload.nativeElement.href = URL.createObjectURL(toExport);
+    this.exportDownload.nativeElement.download = "smartime.json"
+    this.exportDownload.nativeElement.click();
+  }
 }
